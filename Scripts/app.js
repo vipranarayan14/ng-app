ddfd3a546b827208ea1913ad8dcc7ffe5c619046
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-06-09 19:42:26 +0530

    add dynamically createdOn for review entry

--- Scripts/app.js
+++ Scripts/app.js
@@ -64,6 +64,7 @@
         this.review = {};
 
         this.addReview = function(product) {
+          this.review.createdOn = Date.now();
           product.reviews.push(this.review);
           this.review = {};
         };
