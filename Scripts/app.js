4edf649f82a0c1113d61be84dffc1c833c410e3a
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-06-16 23:02:30 +0530

    Moved products to Scripts/products.json

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,73 +1,26 @@
 /*global angular */
-(function () {
-    var app = angular.module('gemStore', ['directives']);
+(function() {
+  var app = angular.module('gemStore', ['directives']);
 
-    var gems = [
-    {
-        name: 'Emerald',
-        price: '4000',
-        description: 'This is a green gem.',
-        image: 'Images/eme.jpg',
-        canPurchase: true,
-        soldOut: false,
-        reviews: [{
-        stars: 5,
-        body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "This gem is so greeeenyyy!!!",
-        author: "tim@example.org",
-        createdOn: 1397490980837
-      }]
-    },
+  app.controller('StoreController', ['$http', function($http) {
+    var store = this;
 
-    {
-        name: 'Sapphire',
-        price: '7000',
-        description: 'This is a blue gem.',
-        image: 'Images/sap.jpg',
-        canPurchase: false,
-        soldOut: false,
-        reviews: [{
-        stars: 5,
-        body: "This is so awesome.",
-        author: "mob@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Not that nice...",
-        author: "pat@example.org",
-        createdOn: 1397490980837
-      }]
-    }
-
-    ];
-
-
-    app.controller('StoreController', function () {
-        this.products = gems;
+    store.products = [];
+    $http.get('Scripts/products.json').success(function (data) {
+      store.products = data;
+      console.log(data);
     });
+  }]);
 
-    app.controller('PanelController', function () {
-        this.tab = 1;
-        this.selectTab = function (setTab) {
-            this.tab = setTab;
-        };
-        this.isSelected = function (checkTab) {
-            return this.tab === checkTab;
-        };
-    });
 
-    app.controller('ReviewController', function() {
-        this.review = {};
+  app.controller('ReviewController', function() {
+    this.review = {};
 
-        this.addReview = function(product) {
-          this.review.createdOn = Date.now();
-          product.reviews.push(this.review);
-          this.review = {};
-        };
-    });
+    this.addReview = function(product) {
+      this.review.createdOn = Date.now();
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
 })();
--- Scripts/directives.js
+++ Scripts/directives.js
@@ -1,11 +1,28 @@
 (function() {
   var app = angular.module('directives', []);
-  
-  app.directive("productReviews", function() {
-   return {
-     restrict: 'E',
-     templateUrl: "product-reviews.html"
-   };
-});
+
+  app.directive('productReviews', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'Views/product-reviews.html'
+    };
+  });
+
+  app.directive('productTabs', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'Views/product-tabs.html',
+      controller: function() {
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+          this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+          return this.tab === checkTab;
+        };
+      },
+      controllerAs: 'tab'
+    };
+  });
 
 })();
